f155bdd9bcd30d44ea2f2c21eb9ad86e53dc9dfa
pinto921 <pinto921@hotmail.com>
2016-11-11 01:29:57 +0000

    task 1

--- task_01.py
+++ task_01.py
@@ -5,3 +5,30 @@
 
 def simple_lookup(var1, var2):
     return var1[var2]
+
+    """key access denied problem
+
+    Attributes:
+        var1: variable helping
+        var2: variable helping
+
+    Returns:
+        it returns a solution to this problem
+
+    example:
+        >>> simple_lookup([1,2], 4)
+        warning: your index/key doesn't exist.
+        [1,2]
+        >>> simple_lookup({}, 'banana')
+        warning: your index/ key doesn't exist. {}
+"""
+    
+
+    try:
+        return var1[var2]
+    except (IndexError, LookupError):
+
+        print 'Warning: your index key doesnt exist'
+
+        return var1
+    
