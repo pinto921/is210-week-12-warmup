9318cc9372a7395727b6c9b3c98e95fc08b4e5eb
pinto921 <pinto921@hotmail.com>
2016-11-11 01:38:43 +0000

    task 02

--- task_02.py
+++ task_02.py
@@ -10,5 +10,25 @@ class InvalidAgeError(Exception):
     pass
 
 def get_age(birthyear):
+
+    """ an Exception when a condition is not met
+
+    Attributes:
+        birthyear: age
+
+    returns:
+        problem: age is not right
+
+    example:
+        >>> get_age(2099)
+        traceback (most recent call last):
+        file "<stdin>", line 1, in <mosule>
+        __main__. InvalidAgeError
+    """
     age = datetime.datetime.now().year - birthyear
     return age
+    if age < 0:
+        raise InvalidAgeError
+    else:
+        return age
+    
